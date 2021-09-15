fec01ac3914afe6d516ce5d0b2a63001979ec32b
jpb06 <jp.bois.06@outlook.fr>
2021-09-15 16:25:58 +0200

    Main workflow

--- src/workflows/generate-types-from-url.ts
+++ src/workflows/generate-types-from-url.ts
@@ -1,9 +1,27 @@
-import { fetchSwaggerJson } from './logic/fetch-swagger-json-file';
-import { generateTypesDefinitions } from './logic/generate-types-definitions';
-import { validateSchema } from './logic/validate-schema';
+import { fetchSwaggerJson } from '../logic/fetching/fetch-swagger-json-file';
+import { generateTypesDefinitions } from '../logic/ts-generation/generate-types-definitions';
+import { validateSchema } from '../logic/validation/validate-schema';
 
-export const generateTypesFromUrl = async (url: string, outPath: string) => {
-  const data = await fetchSwaggerJson(url);
+export type GenerateTypesFromUrlArguments = {
+  sourceUrl: string;
+  outPath: string;
+  shouldClearOutPath?: boolean;
+  shouldCallEslint?: boolean;
+};
+
+export const generateTypesFromUrl = async ({
+  sourceUrl,
+  outPath,
+  shouldClearOutPath = false,
+  shouldCallEslint = false,
+}: GenerateTypesFromUrlArguments) => {
+  const data = await fetchSwaggerJson(sourceUrl);
   const schema = await validateSchema(data);
-  await generateTypesDefinitions(schema, outPath);
+
+  await generateTypesDefinitions(
+    schema,
+    outPath,
+    shouldClearOutPath,
+    shouldCallEslint,
+  );
 };
