68a02f73a901c5f2f1d8034cf4891af2311c9b17
jpb06 <jp.bois.06@outlook.fr>
2021-09-17 14:57:11 +0200

    Fixing an issue with union types

--- src/logic/json-parsing/get-interface-member-definition.ts
+++ src/logic/json-parsing/get-interface-member-definition.ts
@@ -1,22 +1,32 @@
-import { ApiProperty } from '../../types/swagger-schema.interfaces';
+import { ApiTypeDefinition } from '../../types/swagger-schema.interfaces';
 import { getSchemaName } from './get-schema-name';
+
+const getItemDefinition = (property: ApiTypeDefinition): string => {
+  if (property.type === 'array' && property.items) {
+    if (property.items.$ref !== undefined) {
+      return `Array<${getSchemaName(property.items.$ref)}>`;
+    } else if (property.items.type !== undefined) {
+      return `Array<${property.items.type}>`;
+    }
+  } else if (property.$ref) {
+    return `${getSchemaName(property.$ref)}`;
+  }
+
+  return `${property.type}`;
+};
 
 export const getInterfaceMemberDefinition = (
   propName: string,
   required: Array<string>,
-  { type, $ref, items }: ApiProperty,
+  propertyDescription: ApiTypeDefinition | { oneOf: Array<ApiTypeDefinition> },
 ): string => {
   const prop = `  ${propName}${required.includes(propName) ? '' : '?'}`;
 
-  if (type === 'array' && items) {
-    if (items.$ref !== undefined) {
-      return `${prop}: Array<${getSchemaName(items.$ref)}>;\n`;
-    } else if (items.type !== undefined) {
-      return `${prop}: Array<${items.type}>;\n`;
-    }
-  } else if ($ref) {
-    return `${prop}: ${getSchemaName($ref)};\n`;
+  if ('oneOf' in propertyDescription) {
+    return `${prop}: ${propertyDescription.oneOf
+      .map((el) => getItemDefinition(el))
+      .join(' | ')};\n`;
   }
 
-  return `${prop}: ${type};\n`;
+  return `${prop}: ${getItemDefinition(propertyDescription)};\n`;
 };
