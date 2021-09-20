49752db2eef56de0c2f0fb07528a2f244483e5ac
jpb06 <jp.bois.06@outlook.fr>
2021-09-20 13:23:04 +0200

    [Minor]: Segregating responses types

--- src/logic/ts-generation/get-route-outputs-exports.ts
+++ src/logic/ts-generation/get-route-outputs-exports.ts
@@ -1,10 +1,28 @@
 import { RouteResponse } from '../json-parsing/get-route-responses';
+import { capitalize } from '../util/capitalize';
+
+const generateConsolidatedExports = (
+  array: Array<RouteResponse>,
+  route: string,
+  outcome: 'Success' | 'Error',
+): string => {
+  const condition = (el: RouteResponse): boolean =>
+    el.statusCode.startsWith('2') === (outcome === 'Success');
+  const arr = array.filter(condition).map((el) => el.model);
+
+  return `export type ${route}${outcome} = ${Array.from(new Set(arr)).join(
+    ' | ',
+  )};`;
+};
 
 export const getRouteOutputsExports = (
+  routeName: string,
   responses: Array<RouteResponse>,
-): string =>
-  responses.reduce(
-    (exportsList, { statusCode, model }) =>
-      exportsList + `export type Response${statusCode} = ${model};\n`,
-    '',
-  );
+): string => {
+  const route = capitalize(routeName);
+
+  const success = generateConsolidatedExports(responses, route, 'Success');
+  const error = generateConsolidatedExports(responses, route, 'Error');
+
+  return [success, error].join('\n');
+};
---
+++ src/logic/util/capitalize.ts
@@ -0,0 +1,2 @@
+export const capitalize = (input: string): string =>
+  input.charAt(0).toUpperCase() + input.slice(1);
