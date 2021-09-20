3f3587e6fa4ce7b90dc12639f5fcd9abe4e34c65
jpb06 <jp.bois.06@outlook.fr>
2021-09-20 20:33:31 +0200

    Exposing interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { fetchSwaggerJson } from './logic/fetching/fetch-swagger-json-file';
 import { generateTypesDefinitions } from './logic/ts-generation/generate-types-definitions';
 import { validateSchema } from './logic/validation/validate-schema';
+import { InputSwaggerJson } from './types/input-swagger-json.interface';
+import { ValidatedOpenaApiSchema } from './types/swagger-schema.interfaces';
 
 export { fetchSwaggerJson, validateSchema, generateTypesDefinitions };
+
+export type { InputSwaggerJson, ValidatedOpenaApiSchema };
