35695632563257b0280d0e59adcbb7163c117b5a
jpb06 <jpb.06@outlook.com>
2021-12-12 00:14:17 +0100

    Adding jest-watch-typeahead

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,25 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 
 module.exports = {
-   preset: "ts-jest",
-   coverageReporters: [
-      "json-summary",
-      "text",
-      "lcov"
-   ],
-   collectCoverage: true,
-   collectCoverageFrom: [
-      "src/**/*.{ts,tsx}",
-      "!<rootDir>/node_modules/",
-      "!<rootDir>/dist/",
-      "!<rootDir>/src/index.ts",
-      "!<rootDir>/src/cli/generate-types-from-url.cli.ts",
-      "!<rootDir>/src/workflows/generate-types-from-url.ts",
-      "!<rootDir>/src/tests-related/**"
-   ],
-   modulePathIgnorePatterns: ['<rootDir>/dist']
+  preset: "ts-jest",
+  watchPlugins: [
+    "jest-watch-typeahead/filename",
+    "jest-watch-typeahead/testname",
+  ],
+  coverageReporters: [
+    "json-summary",
+    "text",
+    "lcov"
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "!<rootDir>/node_modules/",
+    "!<rootDir>/dist/",
+    "!<rootDir>/src/index.ts",
+    "!<rootDir>/src/cli/generate-types-from-url.cli.ts",
+    "!<rootDir>/src/workflows/generate-types-from-url.ts",
+    "!<rootDir>/src/tests-related/**"
+  ],
+  modulePathIgnorePatterns: ['<rootDir>/dist']
 };
