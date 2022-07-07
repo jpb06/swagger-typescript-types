df740b97afc7beb851cb864ae2b37830adad6b37
jpb06 <jp.bois.06@outlook.fr>
2022-07-07 20:07:52 +0200

    exporting readSwaggerJsonFile

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import { fetchSwaggerJson } from './logic/fetching/fetch-swagger-json-file';
+import { readSwaggerJsonFile } from './logic/fetching/read-swagger-json-file';
 import { generateTypesDefinitions } from './logic/ts-generation/generate-types-definitions';
 import { validateSchema } from './logic/validation/validate-schema';
 import { InputSwaggerJson } from './types/input-swagger-json.interface';
 import { ValidatedOpenaApiSchema } from './types/swagger-schema.interfaces';
 
-export { fetchSwaggerJson, validateSchema, generateTypesDefinitions };
+export {
+  readSwaggerJsonFile,
+  fetchSwaggerJson,
+  validateSchema,
+  generateTypesDefinitions,
+};
 
 export type { InputSwaggerJson, ValidatedOpenaApiSchema };
